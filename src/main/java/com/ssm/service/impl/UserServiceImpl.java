94c846a613f24a654effe03d76b926c2b04a57b7
hchen10 <hchen10@amaroft.com>
2018-11-14 17:08:20 +0800

    使用springjdbc

--- src/main/java/com/ssm/service/impl/UserServiceImpl.java
+++ src/main/java/com/ssm/service/impl/UserServiceImpl.java
@@ -3,8 +3,13 @@ package com.ssm.service.impl;
 import com.ssm.dao.IUserDao;
 import com.ssm.model.User;
 import com.ssm.service.IUserService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service("userService")
 public class UserServiceImpl implements IUserService {
@@ -12,9 +17,19 @@ public class UserServiceImpl implements IUserService {
     @Autowired
     private IUserDao userDao;
 
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
     @Override
     public User selectUser(long userId) {
-        return this.userDao.selectUser(userId);
+        Map<String,Object> param = new HashMap<String,Object>();
+        param.put("id","1");
+        Map<String,Object> result = namedParameterJdbcTemplate.queryForMap("SELECT * FROM user WHERE id=:id", param);
+//        return this.userDao.selectUser(userId);
+        User user = new User();
+        user.setId(1);
+        user.setUsername(result.get("username").toString());
+        return user;
     }
 
     @Override
