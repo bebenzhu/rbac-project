7856941cfe159791901e3aa4f62e17e0cdb5db97
hchen10 <hchen10@amaroft.com>
2018-12-19 09:44:20 +0800

    整理RestfullServlet请求分发

---
+++ src/test/java/com/ssm/rbac/system/BaseJunit4Test.java
@@ -0,0 +1,14 @@
+package com.ssm.rbac.system;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+// 加载spring配置文件
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:org/spring/spring-context.xml"})
+public class BaseJunit4Test  {
+
+
+}
